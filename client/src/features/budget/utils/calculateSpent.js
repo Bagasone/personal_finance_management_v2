83a9004738b9640df8957809b0622b73dac57bee
Bagasone <bagasoneputra@gmail.com>
2026-06-04 08:40:50 +0700

    feat: add format currency and calculate spent

---
+++ client/src/features/budget/utils/calculateSpent.js
@@ -0,0 +1,8 @@
+const calculateSpent = (expenses, budgetCategoryId) => {
+  return expenses.reduce(
+    (acc, exp) => (budgetCategoryId === exp.categoryId ? acc + exp.amount : acc),
+    0,
+  );
+};
+
+export default calculateSpent;

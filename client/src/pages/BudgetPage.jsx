7561a24211dbdccb11248e6fdac6335174156ad1
Bagasone <bagasoneputra@gmail.com>
2026-06-04 09:16:46 +0700

    feat: add filter to expense, income and budget page

--- client/src/pages/BudgetPage.jsx
+++ client/src/pages/BudgetPage.jsx
@@ -1,4 +1,23 @@
+import { useReducer } from "react";
+
+const filterInitialState = {
+  month: new Date().toISOString().slice(0, 7),
+};
+
+const filterReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_MONTH":
+      return { ...state, month: action.payload };
+    case "RESET":
+      return filterInitialState;
+    default:
+      return state;
+  }
+};
+
 const BudgetPage = () => {
+  const [filter, dispatch] = useReducer(filterReducer, filterInitialState);
+
   return (
     <div>
       <h1>Budget Page</h1>
--- client/src/pages/ExpensePage.jsx
+++ client/src/pages/ExpensePage.jsx
@@ -1,4 +1,26 @@
+import { useReducer } from "react";
+
+const filterInitialState = {
+  month: new Date().toISOString().slice(0, 7),
+  categoryId: null,
+};
+
+const filterReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_MONTH":
+      return { ...state, month: action.payload };
+    case "SET_CATEGORY":
+      return { ...state, categoryId: action.payload };
+    case "RESET":
+      return filterInitialState;
+    default:
+      return state;
+  }
+};
+
 const ExpensePage = () => {
+  const [filters, dispatch] = useReducer(filterReducer, filterInitialState);
+
   return (
     <div>
       <h1>Expense Page</h1>

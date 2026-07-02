5e007e5a09c89f10b68f70eb37b1b9d62984a61d
Bagasone <bagasoneputra@gmail.com>
2026-07-02 15:22:06 +0700

    fix: add and fix string, number, date, select and length validation

--- client/src/utils/validation.js
+++ client/src/utils/validation.js
@@ -1,37 +1,83 @@
 import { EXPENSE_CATEGORIES, INCOME_SOURCES, DEBT_TYPES } from "../constants";
 
 const CATEGORIES_ID = EXPENSE_CATEGORIES.map((cat) => cat.id);
-const SOURCE_STATUS = INCOME_SOURCES.map((src) => src.id);
+const SOURCES_ID = INCOME_SOURCES.map((src) => src.id);
 const DEBT_STATUS = Object.values(DEBT_TYPES);
 
-export const validateDescription = (value, field = "description") => {
-  if (typeof value !== "string" || value.trim() === "") {
-    return `${field} is required`;
+const validateEmptyString = (value, field) => {
+  if (String(value).trim() === "") {
+    return `please enter the ${field}`;
   }
+
+  return null;
+};
+
+const validateLength = (value, field) => {
+  if (String(value).length > 100) {
+    return `${field} can't be more than 100 characters`;
+  }
+
+  return null;
+};
+
+const validateFormatDate = (value, field = "date") => {
+  const regex = /^\d{4}-\d{2}-\d{2}$/;
+
+  if (!regex.test(value)) {
+    return `${field} must be in YYYY-MM-DD format`;
+  }
+
+  return null;
+};
+
+const validateValueDate = (value, field = "date") => {
+  if (isNaN(new Date(value).getTime())) {
+    return `${field} must be valid date value`;
+  }
+
+  return null;
+};
+
+const validateFutureDate = (value, field = "date") => {
+  if (new Date(value) > new Date().setHours(23, 59, 59, 999)) {
+    return `${field} can't be in the future`;
+  }
+
+  return null;
+};
+
+const validateSelect = (value, field, select) => {
+  if (!select.includes(value)) {
+    return `${value} is not valid ${field}`;
+  }
+};
+
+export const validateDescription = (value, field = "description") => {
+  const err1 = validateEmptyString(value, field);
+  if (err1) return err1;
+
+  const err2 = validateLength(value, field);
+  if (err2) return err2;
 
   return null;
 };
 
 export const validateNote = (value, field = "note") => {
-  if (typeof value !== "string") {
-    return `${field} should be valid text`;
-  }
-
-  if (value.split("").length > 100) {
-    return `${field} can't be more than 100 character`;
-  }
+  const err1 = validateLength(value, field);
+  if (err1) return err1;
 
   return null;
 };
 
 export const validateNumber = (value, field = "amount") => {
-  const numVal = Number(value);
+  const err1 = validateEmptyString(value, field);
+  if (err1) return err1;
 
-  if (!Number.isFinite(numVal)) {
+  if (!Number.isFinite(Number(value))) {
     return `${field} must be a valid number`;
   }
 
-  if (numVal <= 0) {
+  if (Number(value) <= 0) {
     return `${field} must be greater than 0`;
   }
 
@@ -42,70 +88,75 @@ export const validateAmountPay = (value, field = "amount pay", remainingAmount) 
   const err1 = validateNumber(value, field);
   if (err1) return err1;
 
-  console.log(value > remainingAmount);
-
-  if (value > remainingAmount) return `${field} can't be more than remaining pay`;
-
-  return null;
-};
-
-const validateFormatDate = (value, field = "date") => {
-  if (isNaN(new Date(value).getTime())) {
-    return `${field} must be valid date format`;
-  }
-
-  return null;
-};
-
-const validateDateInFuture = (value, field = "date") => {
-  if (new Date(value) > new Date().setHours(23, 59, 59, 999)) {
-    return `${field} can't be in the future`;
+  if (value > remainingAmount) {
+    return `${field} can't be more than remaining pay`;
   }
 
   return null;
 };
 
 export const validateDate = (value, field = "date") => {
-  const err1 = validateFormatDate(value, field);
+  const err1 = validateEmptyString(value, field);
   if (err1) return err1;
 
-  const err2 = validateDateInFuture(value, field);
+  const err2 = validateFormatDate(value, field);
   if (err2) return err2;
+
+  const err3 = validateValueDate(value, field);
+  if (err3) return err3;
+
+  const err4 = validateFutureDate(value, field);
+  if (err4) return err4;
 
   return null;
 };
 
 export const validateMonth = (value, field = "month") => {
-  if (!/^\d{4}-\d{2}$/.test(value)) return `${field} must be in YYYY-MM format`;
+  const err1 = validateEmptyString(value, field);
+  if (err1) return err1;
+
+  const err2 = validateFormatDate(`${value}-01`, field);
+  if (err2) return err2;
 
   return null;
 };
 
 export const validateCategory = (value, field = "category") => {
-  if (value === "") return `please select a ${field}`;
-  if (!CATEGORIES_ID.includes(value)) return `${field} is not valid`;
+  const err1 = validateEmptyString(value, field);
+  if (err1) return err1;
+
+  const err2 = validateSelect(value, field, CATEGORIES_ID);
+  if (err2) return err2;
 
   return null;
 };
 
 export const validateSource = (value, field = "source") => {
-  if (value === "") return `please select a ${field}`;
-  if (!SOURCE_STATUS.includes(value)) return `${field} is not valid`;
+  const err1 = validateEmptyString(value, field);
+  if (err1) return err1;
+
+  const err2 = validateSelect(value, field, SOURCES_ID);
+  if (err2) return err2;
 
   return null;
 };
 
 export const validateType = (value, field = "type") => {
-  if (value === "") return `please select a ${field}`;
-  if (!DEBT_STATUS.includes(value)) return `${field} is not valid`;
+  const err1 = validateEmptyString(value, field);
+  if (err1) return err1;
+
+  const err2 = validateSelect(value, field, DEBT_STATUS);
+  if (err2) return err2;
 
   return null;
 };
 
 export const validateDueDate = (value, field = "due date") => {
-  if (isNaN(new Date(value).getTime())) {
-    return `${field} must be valid date format`;
-  }
+  const err1 = validateFormatDate(value, field);
+  if (err1) return err1;
+
+  const err2 = validateValueDate(value, field);
+  if (err2) return err2;
 
   if (new Date(value) <= Date.now()) {
     return `${field} can't be now or past`;
@@ -123,7 +174,7 @@ export const validateExpense = ({ amount, description, categoryId, date }) => {
   const err2 = validateDescription(description, "description");
   if (err2) errors.description = err2;
 
-  const err3 = validateCategory(categoryId);
+  const err3 = validateCategory(categoryId, "category");
   if (err3) errors.categoryId = err3;
 
   const err4 = validateDate(date, "date");
@@ -164,10 +215,10 @@ export const validateBudget = ({ limit, categoryId, month }) => {
   const err1 = validateNumber(limit, "limit");
   if (err1) errors.limit = err1;
 
-  const err2 = validateCategory(categoryId);
+  const err2 = validateCategory(categoryId, "category");
   if (err2) errors.categoryId = err2;
 
-  const err3 = validateMonth(month);
+  const err3 = validateMonth(month, "month");
   if (err3) errors.month = err3;
 
   if (Object.keys(errors).length) {
@@ -190,7 +241,7 @@ export const validateDebt = ({ totalAmount, description, type, dueDate }) => {
   if (err3) errors.type = err3;
 
   if (dueDate) {
-    const err4 = validateFormatDate(dueDate, "due date");
+    const err4 = validateDueDate(dueDate, "due date");
     if (err4) errors.dueDate = err4;
   }
 

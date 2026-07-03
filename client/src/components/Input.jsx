8b3243cfaee8f47953cca33741396754dbb71e04
Bagasone <bagasoneputra@gmail.com>
2026-07-03 09:10:49 +0700

    refactor: add forwardRef to Input component

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,8 +1,11 @@
-const Input = ({ type = "text", label, id, value, onChange, error }) => {
+import { forwardRef } from "react";
+
+const Input = forwardRef(({ type = "text", label, id, value, onChange, error }, ref) => {
   return (
     <div className="flex flex-col text-sm">
       {label && <label htmlFor={id}>{label}</label>}
       <input
+        ref={ref}
         type={type}
         name={id}
         id={id}
@@ -13,6 +16,6 @@ const Input = ({ type = "text", label, id, value, onChange, error }) => {
       {error && <p className="text-rose-500 first-letter:capitalize">{error}</p>}
     </div>
   );
-};
+});
 
 export default Input;

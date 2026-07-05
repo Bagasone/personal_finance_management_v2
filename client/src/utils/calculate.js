e0228b6deb73f7c4be7e5d31d23cf0311eda70a9
Bagasone <bagasoneputra@gmail.com>
2026-07-05 19:35:21 +0700

    fix: calculate utils with key and filter calculation

--- client/src/utils/calculate.js
+++ client/src/utils/calculate.js
@@ -1,8 +1,8 @@
-export const calculate = (data, filter = null) => {
+export const calculate = (data, key = "amount", filter = null) => {
   if (!data) return 0;
-  else if (!filter) return data.reduce((acc, curr) => acc + curr.amount, 0);
+  else if (!filter) return data.reduce((acc, curr) => acc + curr[key], 0);
   return data.reduce((acc, curr) => {
-    return curr[filter.label] === filter.value ? acc + curr.amount : acc;
+    return curr[filter.key] === filter.id ? acc + curr[key] : acc;
   }, 0);
 };
 

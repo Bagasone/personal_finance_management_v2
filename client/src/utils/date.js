37b9455751a9036100ba12b0609c4d9eb52d1cba
Bagasone <bagasoneputra@gmail.com>
2026-07-07 08:25:28 +0700

    feat: add previous monht function

--- client/src/utils/date.js
+++ client/src/utils/date.js
@@ -1,3 +1,8 @@
 export const getFullDate = () => new Date().toISOString();
 export const getDate = () => new Date().toISOString().split("T")[0];
 export const getMonth = () => new Date().toISOString().slice(0, 7);
+export const getPreviousMonth = (selectedDate) => {
+  const currentMonth = new Date(selectedDate);
+  currentMonth.setMonth(currentMonth.getMonth() - 1);
+  return currentMonth.toISOString().slice(0, 7);
+};

ea089277b291f4c161a0eb2d1d79f697dda9f46c
Bagasone <bagasoneputra@gmail.com>
2026-07-21 14:09:32 +0700

    feat: add prevMont function

--- client/src/utils/date.js
+++ client/src/utils/date.js
@@ -1,3 +1,10 @@
 export const getFullDate = () => new Date().toISOString();
 export const getDate = () => new Date().toISOString().split("T")[0];
 export const getMonth = () => new Date().toISOString().slice(0, 7);
+export const prevMonth = (month) => {
+  const date = new Date(`${month}-01`);
+  date.setMonth(date.getMonth() - 1);
+
+  const prev_month = date.toISOString().slice(0, 7);
+  return prev_month;
+};

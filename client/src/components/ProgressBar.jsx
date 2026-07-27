16bc48e963a08c326529e7b9d3639ac40b902354
Bagasone <bagasoneputra@gmail.com>
2026-07-27 11:32:27 +0700

    fix: add fallback 0 when percent null

--- client/src/components/ProgressBar.jsx
+++ client/src/components/ProgressBar.jsx
@@ -2,8 +2,8 @@ const ProgressBar = ({ fill, color }) => {
   return (
     <div className="flex justify-start items-center rounded-sm bg-black-300 w-full h-3">
       <div
-        style={{ width: `${fill}%` }}
-        className={`rounded-sm h-3 ${color}`}></div>
+        style={{ width: `${fill ?? 0}%` }}
+        className={`rounded-sm h-2.5 ${color}`}></div>
     </div>
   );
 };
